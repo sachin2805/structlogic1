a0d9c51ec248ca34e69c1540f655731bbf401d44
sachin2805 <devpowaras123@gmail.com>
2021-07-26 19:12:22 +0530

    Initial commit

---
+++ struct.c
@@ -0,0 +1,398 @@
+Example 1:
+struct employee
+{
+    char employeename[50];
+    int citNo;
+    float salary;
+};
+
+Example 2:
+struct gastank
+{
+    char companyname[50];
+    int weight;
+    float price;
+};
+
+Example 3:
+struct cooker
+{
+  char brand[20];
+int size ;
+float price ;
+char color[10];
+char material[10];
+};
+
+Example 4:
+struct colgate
+{
+  char brand[20];
+int gm ;
+float price ;
+char color[10];
+}
+
+Example 5:
+struct mobile 
+{
+  char brand[20];
+int ram;
+float price ;
+int storage;
+};
+
+Example 6:
+struct light
+{
+  char brand[20];
+int watt;
+float price ;
+char validaty[10];
+};
+
+Example 7:
+
+struct hairdrier
+{
+  char brand[20];
+int watt ;
+char type[20] ;
+float price;
+};
+
+Example 8:
+struct stud
+{
+  char snmae[20];
+int srn ;
+float fees ;
+};
+
+Example 9:
+struct gel
+ {
+  char brand[20];
+int validity ;
+float price ;
+};
+
+
+Example 10:
+
+struct notebook 
+{
+  char brand[20];
+int size ;
+float price ;
+};
+
+Example 11:
+struct door 
+{
+  char brand[20];
+int length ;
+float price ;
+};
+
+Example 12:
+struct window
+{
+  char brand[20];
+int length ;
+float price ;
+};
+
+Example 13:
+struct glass
+{
+  char brand[20];
+int length ;
+float price ;
+};
+
+Example 14:
+struct building {
+  char name[20];
+int floor ;
+float hight ;
+};
+Example 15:
+
+struct hostel
+ {
+  char name[20];
+int idno ;
+float fees ;
+int age;
+};
+
+Example 16:
+
+struct trimmer 
+{
+  char brand[20];
+int watt;
+float price;
+int batterypackup;
+};
+
+Example 17:
+struct mask 
+{
+  char brand[20];
+int count ;
+float price ;
+};
+
+Example 18
+
+struct perfume
+{
+  char brand[20];
+int length ;
+float price ;
+};
+Example 19:
+
+struct std{
+  char namr[20];
+int id;
+float cgpa ;
+char gender;
+};
+Example 20:
+
+struct newspaper{
+  char name[20];
+int price ;
+float weight;
+};
+
+Example 21:
+
+struct emp{
+ char name[28];
+ int id;
+ float salary;
+};
+ 
+Example 22:
+struct sandle{
+ char color[25];
+ int size;
+ float price ;
+};
+
+Example 23:
+struct freeze{
+ char color[15];
+ int height ;
+ float price ;
+};
+
+Example 24:
+
+struct washing machine {
+ char brand[15];
+ int height ;
+ float price ;
+};
+
+Example 25:
+
+struct kurti{
+ char brand[15];
+ int size ;
+ float price ;
+};
+
+Example 26:
+
+struct bed{
+ char brand[30];
+ int size ;
+ float price ;
+};
+
+Example 27:
+struct chair
+{
+ char brand[30];
+ int size ;
+ float price ;
+ int strenght;
+};
+
+
+Example 28:
+struct hospital {
+ char name[30];
+ int NoOfEmployee ;
+ float salary ;
+};
+
+Example 29:
+struct sofa {
+ char color[30];
+ int size ;
+ float price ;
+};
+
+Example 30:
+struct laptop {
+ char brand[10];
+ int storage ;
+ float price ;
+};
+
+Example 31:
+
+struct house {
+ char name[10];
+ int height ;
+ float price ;
+};
+
+Example 32:
+struct college {
+ char name[10];
+ int students ;
+};
+
+Example 33:
+
+struct nailpaint {
+ char brand[10];
+ char color[15];
+ int price ;
+};
+
+Example 34:
+
+struct family {
+ char name[10];
+ char gender [15];
+ int aadharNo;
+};
+
+Example  35:
+struct school {
+ char name[10];
+ int stdCount;
+ float fees ;
+};
+
+Example 36:
+struct Tiles 
+{
+ char brand[10];
+ int size;
+ float price ;
+};
+
+Example 37:
+struct bike {
+ char brand[10];
+ int average ;
+ float price ;
+};
+
+Example 38:
+struct bag {
+  char brand[20];
+int pocket ;
+float price ;
+};
+
+Example 39:
+struct tv 
+{
+ char brand[30];
+ int size ;
+ float price ;
+};
+
+Example 40:
+struct shart 
+{
+  char brand[20];
+  int size ;
+  float price ;
+};
+
+Example 41:
+struct Jean {
+  char brand[20];
+int size ;
+float price ;
+};
+Example 42:
+struct curtain
+{
+  char colour[20];
+int price;
+float length ;
+};
+Example 43:
+struct vegetables 
+{
+  char name[20];
+int weight ;
+float price ;
+};
+Example 44:
+struct chicken 
+{
+  char type[20];
+int weight ;
+float price ;
+};
+Example 45:
+struct tree
+{
+  char type[20];
+int count ;
+float hight ;
+};
+Example 46:
+
+struct hair
+{
+  char colour[20];
+int length ;
+char type[20] ;
+};
+Example 47:
+
+struct shoes
+ {
+  char colour[20];
+  int price;
+  float size ;
+};
+Example 48:
+
+struct soap 
+{
+  char brand[20];
+int weight ;
+float price ;
+};
+Example 49:
+struct belt 
+{
+  char brand[20];
+int weight ;
+float price ;
+char material[10];
+};
+Example 50:
+struct handrumal 
+{
+  char brand[20];
+int weight ;
+float price ;
+char material[10];
+};
+
